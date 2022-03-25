d44812f910b33328b2fc620e90e226f7ec701fde
Adam Champion <championone@gmail.com>
2022-03-25 10:48:39 -0400

    Changed Google Maps renderer to latest version

---
+++ app/src/main/kotlin/edu/ohiostate/whereamikotlin/MapsApplication.kt
@@ -0,0 +1,31 @@
+package edu.ohiostate.whereamikotlin
+
+import android.app.Application
+import android.util.Log
+import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.OnMapsSdkInitializedCallback
+import edu.ohiostate.whereamikotlin.MapsApplication
+
+class MapsApplication : Application(), OnMapsSdkInitializedCallback {
+    override fun onCreate() {
+        super.onCreate()
+        MapsInitializer.initialize(applicationContext, MapsInitializer.Renderer.LATEST, this)
+    }
+
+    override fun onMapsSdkInitialized(renderer: MapsInitializer.Renderer) {
+        when (renderer) {
+            MapsInitializer.Renderer.LATEST -> Log.d(
+                TAG,
+                "The latest version of the renderer is used."
+            )
+            MapsInitializer.Renderer.LEGACY -> Log.d(
+                TAG,
+                "The legacy version of the renderer is used."
+            )
+        }
+    }
+
+    companion object {
+        private val TAG = MapsApplication::class.java.simpleName
+    }
+}
